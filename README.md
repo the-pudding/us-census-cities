4ff13bd25ac20f166a5a910d4479b28b47b29f47
russellsamora <russellsamora@gmail.com>
2022-12-14 15:24:13 -0500

    refining

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 * https://www.census.gov/data/tables/time-series/demo/popest/2020s-total-cities-and-towns.html#ds
 * https://www2.census.gov/programs-surveys/popest/technical-documentation/file-layouts/2020-2021/SUB-EST2021.pdf
 * https://www.census.gov/programs-surveys/popest/guidance-geographies/terms-and-definitions.html
-
+* [countries](https://raw.githubusercontent.com/umpirsky/country-list/master/data/en_US/country.csv)
+* [cities](https://simplemaps.com/data/world-cities)
--- index.js
+++ index.js
@@ -5,67 +5,104 @@ const COLUMN_POP = "POPESTIMATE2021";
 
 const raw = d3.csvParse(fs.readFileSync("./input/sub-est2021_all.csv", "utf8"));
 
-const states = raw.filter(d => d.SUMLEV === "040");
+const states = raw.filter((d) => d.SUMLEV === "040");
 
-const remove = ({ SUMLEV, NAME }) => {
-	// remove state, county, conslidated city within city SUMLEV
-	const deny = ["040", "050", "172"];
-	if (deny.includes(SUMLEV)) return false;
-	if (NAME.includes("(pt.)")) return false;
-	if (NAME.includes("Balance of") || NAME.includes("(balance)")) return false;
-	if (NAME.endsWith(" Reservation")) return false;
-	if (NAME.includes(" government")) return false;
-	if (NAME.startsWith("Township ")) return false;
-	if (NAME.endsWith(" gore")) return false;
-	if (NAME.endsWith(" location")) return false;
-	if (NAME.endsWith(" urban county")) return false;
-	if (NAME.endsWith(" corporation")) return false;
+const remove = ({ SUMLEV, NAME, FUNCSTAT, STNAME }) => {
+  // remove state, county, conslidated city within city SUMLEV
+  const deny = ["040", "050", "172"];
+  if (FUNCSTAT !== "A") return false;
+  if (deny.includes(SUMLEV)) return false;
+  if (NAME.includes("(pt.)")) return false;
+  if (NAME.includes("Balance of") || NAME.includes("(balance)")) return false;
+  if (NAME.endsWith(" Reservation")) return false;
+  if (NAME.includes(" government")) return false;
+  if (NAME.startsWith("Township ")) return false;
+  if (NAME.endsWith(" gore")) return false;
+  if (NAME.endsWith(" location")) return false;
+  if (NAME.endsWith(" urban county")) return false;
+  if (NAME.endsWith(" corporation")) return false;
+  if (NAME.endsWith(" village") && ["Vermont"].includes(STNAME)) return false;
+  if (
+    NAME.endsWith(" township") &&
+    !["Michigan", "New Jersey", "Pennsylvania"].includes(STNAME)
+  )
+    return false;
 
-	
-	return true;
+  return true;
 };
 
-const refineName = (name) => {
-	const ends = ["city and borough", "city", "township", "town", "village", "borough", "County", "UT", "plantation", "reservation", "Parish", "Borough", "municipality", "grant", "purchase", "CDP"];
-	const starts = ["Urban "]
-	ends.forEach(end => {
-		const e = ` ${end}`;
-		if (name.endsWith(e)) name = name.replace(e, "");
-	});
+const refineName = ({ STATE, NAME }) => {
+  let name = NAME;
+  const ends = [
+    "city and borough",
+    "city",
+    "metro township",
+    "charter township",
+    "charter",
+    "township",
+    "town",
+    "village",
+    "borough",
+    "County",
+    "UT",
+    "plantation",
+    "reservation",
+    "Parish",
+    "Borough",
+    "municipality",
+    "grant",
+    "purchase",
+    "CDP",
+  ];
+  const starts = ["Urban "];
 
-	starts.forEach(start => {
-		if (start.startsWith(start)) name = name.replace(start, "");
-	});
-	return name;
+  ends.forEach((end) => {
+    const e = ` ${end}`;
+    if (name.endsWith(e)) name = name.replace(e, "");
+  });
+
+  starts.forEach((start) => {
+    if (start.startsWith(start)) name = name.replace(start, "");
+  });
+
+  // special cases
+  if (name.endsWith(" Town") && STATE === "25")
+    name = name.replace(" Town", "");
+
+  return name;
 };
 
 const cleanCols = (d) => {
-	const population = d[COLUMN_POP];
-	const name = refineName(d.NAME);
-	const id_county = `${d.STATE}_${d.COUNTY}_${name}`;
-	const id_pop = `${d.STATE}_${name}_${population}`;
-	const o = {
-		id_county, 
-		id_pop,
-		name,
-		population
-	};
+  const population = d[COLUMN_POP];
+  const name = refineName(d);
+  const id_pop = `${d.STATE}_${name}_${population}`;
+  // const id_county = `${d.STATE}_${d.COUNTY}_${name}`;
+  // const id_place = `${d.STATE}_${d.PLACE}_${population}`;
+  const id = id_pop;
+  const o = {
+    id,
+    name,
+    population,
+  };
 
-	const keep = ["SUMLEV", "STATE", "COUNTY", "NAME"];
-	keep.map(col => o[col] = d[col]);
-		
-	return o;
+  const keep = ["SUMLEV", "STATE", "COUNTY", "NAME", "STNAME", "FUNCSTAT"];
+  keep.map((col) => (o[col] = d[col]));
+
+  return o;
 };
 
-const clean = raw
-	.filter(remove)
-	.map(cleanCols);
+const clean = raw.filter(remove).map(cleanCols);
 
 // grab the first one of the grouping since they represent the same place to reduce duplicates
-const deduped = d3.groups(clean, d => d.id_pop).map(d => d[1][0]);
+const deduped = d3
+  .groups(clean, (d) => d.id)
+  .map((d) => {
+    d[1].sort((a, b) => d3.descending(a.population, b.population));
+    return d[1][0];
+  });
 
-const sumCities = d3.sum(deduped, d => d.population);
-const sumStates = d3.sum(states, d => +d[COLUMN_POP]);
+const sumCities = d3.sum(deduped, (d) => d.population);
+const sumStates = d3.sum(states, (d) => +d[COLUMN_POP]);
 
 console.log("row count:", d3.format(",")(deduped.length));
 console.log("city pop: ", d3.format(",")(sumCities));
